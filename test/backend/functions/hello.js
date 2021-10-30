847b154ef7881032e2ca8964bb70d3536cea6148
Johan Eliasson <johan@nhost.io>
2021-10-30 18:27:35 +0200

    updated function

--- test/backend/functions/hello.js
+++ test/backend/functions/hello.js
@@ -1,8 +1,4 @@
 module.exports = (req, res) => {
-  const { name = 'World' } = req.query;
-  console.log(
-    'This logs should be printed in dev and stored somewhere in prod'
-  );
-  console.log('NEW LOG 123');
-  return res.status(200).send(`UPDATED 123 ${name}!`);
+  const { name = 'World' } = req.post;
+  res.status(200).send(`Hello ${name}!`);
 };
