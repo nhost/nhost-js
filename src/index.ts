23ba88082d8eddf19f749fdd2e55229349732f66
Mrinal Wahal <mrinal_w@rocketmail.com>
2022-01-08 22:20:40 +0530

    exported create-client function

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
+import { NhostClient } from '.';
+import { NhostClientConstructorParams } from '.';
+
+const createClient = (config: NhostClientConstructorParams) => {
+    return new NhostClient(config);
+};  
+
 export * from './core';
+export {createClient};
